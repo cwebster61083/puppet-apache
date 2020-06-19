80bb4bc4a9f1738ee86b2430be2b4a70d654130d
Chris Webster <chirs.webster@puppet.com>
2020-06-19 15:47:16 -0700

    asdf

--- manifests/dissite.pp
+++ manifests/dissite.pp
@@ -9,7 +9,7 @@ class apache::dissite(
 ) {
   if $facts['os']['family'] == 'Debian' {
     exec { "a2dissite ${vhost_file}":
-      onlyif => "test -f /etc/apache2/sites-enable/${vhost_file}",
+      onlyif => "test -f /etc/apache2/sites-enabled/${vhost_file}",
         path => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
       notify => Service[$apache::service_name],
     }
